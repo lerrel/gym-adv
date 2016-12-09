bf4ece61a73728fb8ec9cf40b159d6848ff51929
Lerrel Pinto <lerrel.pinto@gmail.com>
2016-12-08 19:28:40 -0500

    openAI gym for adversarial environments

---
+++ gym/envs/adversarial/classic_control/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.adversarial.classic_control.cartpole import CartPoleEnv
+from gym.envs.adversarial.classic_control.mountain_car import MountainCarEnv
+from gym.envs.adversarial.classic_control.continuous_mountain_car import Continuous_MountainCarEnv
+from gym.envs.adversarial.classic_control.pendulum import PendulumEnv
+from gym.envs.adversarial.classic_control.acrobot import AcrobotEnv
+
